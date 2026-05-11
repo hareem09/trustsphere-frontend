de2483cd4ac1a18787ca0d375c14b105ef82f929
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2026-05-11 22:02:03 +0500

    Refactor Layout component for dark mode styles
    Updated main layout styles for dark mode support.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,17 +3,14 @@ import SideBar from './components/SideBar';
 import  { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 function Layout() {
-  const [mode ,setMode] = useState('light')
-   const toggleMode = () =>{
-    setMode(mode === 'light' ?'dark ' :'light')
-   
+
   }
   return (
     <>
     <div className='md:flex '>
      <SideBar />
      
-     <main className='w-full bg-gray-200 ' >
+     <main className='w-full bg-gray-300 dark:bg-gray-800 dark:text-white ' >
       <NavBar/>
         <Outlet />
      </main>
