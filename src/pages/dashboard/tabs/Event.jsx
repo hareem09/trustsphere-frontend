145353833f0ad70e375b313a4e6158be0daf135b
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2026-05-11 22:00:01 +0500

    updated

--- src/pages/dashboard/tabs/Event.jsx
+++ src/pages/dashboard/tabs/Event.jsx
@@ -10,7 +10,7 @@ function Event() {
     description: "",
   });
   const [isEditing, setIsEditing] = useState(false);
-  const [mode ,setMode] = useState('light')
+
   useEffect(() => {
     const storedData = localStorage.getItem("eventData");
     if (storedData) {
@@ -79,25 +79,14 @@ function Event() {
     localStorage.setItem("eventData", JSON.stringify(filteredData));
   };
 
-  const toggleMode = () =>{
-    setMode(mode === 'light' ?'dark ' :'light')
-   
-  }
+  
   return(
   <>
-    <main className=" ml-4 h-lvh" className={
-      mode === 'light'
-        ? 'bg-gray-200 text-black'
-        : 'bg-gray-800 text-gray-200 '
-    }>
+    <main className=" md:ml-4  bg-gray-300 dark:bg-gray-800 text-black dark:text-white">
       <h2 className="text-center font-bold text-2xl">Event Management</h2>
       <h3 className="font-bold">Records:</h3>
-      <section className="flex md:justify-center">
-        <table className=" text-sm md:text-lg bg-gray-100 rounded-2xl w-2/4 mt-4 shadow-2xl shadow-gray-600" className={
-      mode === 'light'
-        ? 'bg-gray-100 text-black'
-        : 'bg-gray-800 text-gray-200 '
-    }>
+      <section className="md:flex justify-center">
+        <table className=" rounded-2xl md:w-2/4 mt-4 shadow-2xl shadow-gray-600  bg-gray-300 dark:bg-gray-800 text-black dark:text-white"  >
           <thead >
             <tr className="head   ">
               <th>ID</th>
@@ -107,9 +96,9 @@ function Event() {
               <th>Description</th>
             </tr>
           </thead>
-          <tbody className="md:p-4 md:m-3">
+          <tbody className="md:p-4 md:m-3 text-sm md:text-lg">
             {eventData.map((event) => (
-              <tr key={event.id} className="p-4">
+              <tr key={event.id} className="md:p-4">
                 <td className="md:p-2">{event.id}</td>
                 <td className="md:p-2 text-center">{event.title}</td>
                 <td className="md:p-2">{event.date}</td>
@@ -117,7 +106,7 @@ function Event() {
                 <td className="md:p-2">{event.description}</td>
                 <td>
                   <button
-                    className="p-1 m-1 text-gray-200 rounded-lg border-2 border-sky-300 bg-sky-500 hover:bg-sky-300 cursor-pointer"
+                    className="md:p-1 md:m-1 text-gray-200 rounded-lg border-2 border-sky-300 bg-sky-500 hover:bg-sky-300 cursor-pointer"
                     onClick={() => handleEdit(event.id)}
                   >
                     Edit
@@ -125,7 +114,7 @@ function Event() {
                 </td>
                 <td>
                   <button
-                    className="p-1 m-1 text-gray-200 rounded-lg border-2 border-red-300 bg-red-500 hover:bg-red-300 cursor-pointer"
+                    className="md:p-1 md:m-1 text-gray-200 rounded-lg border-2 border-red-300 bg-red-500 hover:bg-red-300 cursor-pointer"
                     onClick={() => handleDelete(event.id)}
                   >
                     Delete
@@ -139,7 +128,7 @@ function Event() {
       <h3 className="font-bold mt-4">Add New Event:</h3>
       <form
         onSubmit={handleSubmit}
-        className="w-full md:flex justify-center flex-wrap"
+        className="w-full flex md:flex-row  flex-col md:justify-center flex-wrap"
       >
         <div className="md:w-1/2 flex items-center flex-col gap-4 mt-4 ">
           <label htmlFor="id">ID:</label>
@@ -192,19 +181,21 @@ function Event() {
             className="w-1/2 p-2 border-2 border-gray-400 bg-gray-100 outline-0"
           />
         </div>
+        <div className="md:w-1/2 flex items-center flex-col gap-4 mt-4 ">
         <button
           type="submit"
-          className="border-2 border-blue-500 bg-blue-300 p-3 hover:bg-blue-200 "
+          className="border-2 border-blue-500 bg-blue-300 p-3 hover:bg-blue-200  "
         >
           Add Event
         </button>
+        </div>
       </form>
-       <button onClick={toggleMode}>
-          {mode === 'light' ? 'Dark Mode' : 'Light Mode'}
-        </button>
+       
+        
     </main>
 </>
 )
 }
 
 export default Event;
+
